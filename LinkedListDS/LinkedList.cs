23c7d1a857609b8b518ec3b72c72de577b1f5d1d
AkshayPoriya <akshayporiya1234@gmail.com>
2020-10-25 16:24:16 +0530

    [Akshay] UC1 Create a simple LinkedList

---
+++ LinkedListDS/LinkedList.cs
@@ -0,0 +1,71 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LinkedList.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Akshay Poriya"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace LinkedListDS
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// LinkedList Class
+    /// </summary>
+    /// <typeparam name="T"> Generic Type</typeparam>
+    public class LinkedList<T>
+    {
+        /// <summary>
+        /// The head Node
+        /// </summary>
+        public Node<T> head;
+
+        /// <summary>
+        /// The tail Node
+        /// </summary>
+        public Node<T> tail;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinkedList{T}"/> class.
+        /// </summary>
+        public LinkedList()
+        {
+            this.head = null;
+            this.tail = this.head;
+        }
+
+        /// <summary>
+        /// Adds the Node.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public void AddNode(T data)
+        {
+            if (this.head == null)
+            {
+                this.head = new Node<T>(data);
+                this.tail = this.head;
+                return;
+            }
+
+            Node<T> temp = new Node<T>(data);
+            this.tail.next = temp;
+            this.tail = temp;
+        }
+
+        /// <summary>
+        /// Traverses this instance.
+        /// </summary>
+        public void Traverse()
+        {
+            Node<T> temp = this.head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LinkedListDS/Program.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Akshay Poriya"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace LinkedListDS
+{
+    using System;
+
+    /// <summary>
+    /// Program class contains Main function
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.Traverse();
+            linkedList.AddNode(56);
+            linkedList.Traverse();
+            linkedList.AddNode(30);
+            linkedList.Traverse();
+            linkedList.AddNode(70);
+            linkedList.Traverse();
+        }
+    }
+}
